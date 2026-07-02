d9000a396bab128684d7829f750acef93b44f611
Supriya Bajpai <2k23.csdsc2311724@gmail.com>
2026-07-02 22:36:38 +0530

    Remove all files

--- matchroutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/match.controller");
-
-router.post("/resume", controller.saveResume);
-router.get("/match", controller.matchInternships);
-
-module.exports = router;
--- user.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/user.controller");
-
-router.post("/register", controller.register);
-router.post("/login", controller.login);
-
-module.exports = router;
